c117f3efcfd425df2711f4bd0bfed5368df83198
Michael Mohr <akihana@gmail.com>
2012-02-13 11:48:07 -0800

    rebuild with NDK v7 and gmp-5.0.4

--- compile-gmp.sh
+++ compile-gmp.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
+# TODO: cross-compile testsuite & run on a target device
+# Building the tests without running them is easy; just do "make TESTS=check"
+
 if [ ! -x configure ]
 then
   echo "Run this script from the GMP base directory"
   exit 1
-fi
-
-# bugfix for GMP 5.0.2, published on gmplib.org
-if [ ! -f ANDROID_PATCHED ]
-then
-  patch -p1 < $0
-  touch ANDROID_PATCHED
 fi
 
 export NDK="/home/dev/android-ndk-r7"
@@ -21,7 +17,7 @@ then
   exit 1
 fi
 
-# Extract an android-5 toolchain if needed
+# Extract an android-9 toolchain if needed
 export TARGET="android-9"
 export TOOLCHAIN="/tmp/${TARGET}"
 if [ ! -d ${TOOLCHAIN} ]
@@ -61,43 +57,3 @@ cd armeabi && mv usr/lib/libgmp.so usr/include/gmp.h . && rm -rf usr && cd ..
 make distclean
 
 exit 0
-
-################################################################################################################
-
-# HG changeset patch
-# User Torbjorn Granlund <tege@gmplib.org>
-# Date 1310730221 -7200
-# Node ID 538dfce27f410b910d5e2f011119269e224d16a3
-# Parent  03ed209dd7efd4f4fff0ce297bb3a8f7e7ba2366
-(mpn_dcpi1_bdiv_q): Get mpn_sub_1 size argument right.
-
-diff -r 03ed209dd7ef -r 538dfce27f41 mpn/generic/dcpi1_bdiv_q.c
---- a/mpn/generic/dcpi1_bdiv_q.c	Thu Jun 16 12:22:24 2011 +0200
-+++ b/mpn/generic/dcpi1_bdiv_q.c	Fri Jul 15 13:43:41 2011 +0200
-@@ -7,7 +7,7 @@
-    SAFE TO REACH THEM THROUGH DOCUMENTED INTERFACES.  IN FACT, IT IS ALMOST
-    GUARANTEED THAT THEY WILL CHANGE OR DISAPPEAR IN A FUTURE GMP RELEASE.
- 
--Copyright 2006, 2007, 2009, 2010 Free Software Foundation, Inc.
-+Copyright 2006, 2007, 2009, 2010, 2011 Free Software Foundation, Inc.
- 
- This file is part of the GNU MP Library.
- 
-@@ -28,7 +28,6 @@
- #include "gmp-impl.h"
- 
- 
--
- mp_size_t
- mpn_dcpi1_bdiv_q_n_itch (mp_size_t n)
- {
-@@ -130,7 +129,7 @@
-       qn = nn - qn;
-       while (qn > dn)
- 	{
--	  mpn_sub_1 (np + dn, np + dn, qn, cy);
-+	  mpn_sub_1 (np + dn, np + dn, qn - dn, cy);
- 	  cy = mpn_dcpi1_bdiv_qr_n (qp, np, dp, dn, dinv, tp);
- 	  qp += dn;
- 	  np += dn;
-
