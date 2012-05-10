f5573a4af8bca6892f515b495cc8d0f7ed11f347
Michael Mohr <akihana@gmail.com>
2012-05-09 20:20:34 -0700

    some work on auto-inclusion of the NEON shared library

--- Android.mk
+++ Android.mk
@@ -1,6 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := gmp
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libgmp.so
+
+# possible future inclusion of the NEON shared object?
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+  LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libgmp.so
+else
+  LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libgmp.so
+endif
+
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(TARGET_ARCH_ABI)
 include $(PREBUILT_SHARED_LIBRARY)
