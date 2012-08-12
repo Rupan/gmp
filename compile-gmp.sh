a13c10862edb304b3781222fd6c922e202415be3
Michael Mohr <akihana@gmail.com>
2012-08-11 18:46:47 -0700

    Update the build script to make use of the Android NDK r8b

--- compile-gmp.sh
+++ compile-gmp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # TODO: cross-compile testsuite & run on a target device
-# Building the tests without running them is easy; just do "make TESTS=check"
+# Building the tests without running them is easy; just do "make check TESTS=check"
 
 if [ ! -x configure ]
 then
@@ -9,7 +9,7 @@ then
   exit 1
 fi
 
-export NDK="/home/dev/android-ndk-r7"
+export NDK="${HOME}/Downloads/android-ndk-r8b"
 if [ ! -d ${NDK} ]
 then
   echo "Please download and install the NDK, then update the path in this script."
@@ -22,36 +22,39 @@ export TARGET="android-9"
 export TOOLCHAIN="/tmp/${TARGET}"
 if [ ! -d ${TOOLCHAIN} ]
 then
-  ${NDK}/build/tools/make-standalone-toolchain.sh --platform=${TARGET} --install-dir=${TOOLCHAIN}
+  ${NDK}/build/tools/make-standalone-toolchain.sh --toolchain=arm-linux-androideabi-4.6 --platform=${TARGET} --install-dir=${TOOLCHAIN}
 fi
 
-export PATH=${TOOLCHAIN}/bin:$PATH
+export PATH="${TOOLCHAIN}/bin:${PATH}"
+# export LDFLAGS='-Wl,--fix-cortex-a8,-z,noexecstack,-z,relro'
 export LDFLAGS='-Wl,--fix-cortex-a8'
 export LIBGMP_LDFLAGS='-avoid-version'
 export LIBGMPXX_LDFLAGS='-avoid-version'
 
 ################################################################################################################
 
+BASE_CFLAGS='-O2 -pedantic -fomit-frame-pointer'
+
 # armeabi-v7a with neon (unsupported target: will cause crashes on many phones, but works well on the Nexus One)
-export CFLAGS="-O2 -pedantic -fomit-frame-pointer -march=armv7-a -mfloat-abi=softfp -mfpu=neon -ftree-vectorize -ftree-vectorizer-verbose=2"
+export CFLAGS="${BASE_CFLAGS} -march=armv7-a -mfloat-abi=softfp -mfpu=neon -ftree-vectorize -ftree-vectorizer-verbose=2"
 ./configure --prefix=/usr --disable-static --build=i686-pc-linux-gnu --host=arm-linux-androideabi
-make -j8 2>&1 | tee armeabi-v7a-neon.log
+make -j8 V=1 2>&1 | tee armeabi-v7a-neon.log
 make install DESTDIR=$PWD/armeabi-v7a-neon
 cd armeabi-v7a-neon && mv usr/lib/libgmp.so usr/include/gmp.h . && rm -rf usr && cd ..
 make distclean
 
 # armeabi-v7a
-export CFLAGS="-O2 -pedantic -fomit-frame-pointer -march=armv7-a -mfloat-abi=softfp"
+export CFLAGS="${BASE_CFLAGS} -march=armv7-a -mfloat-abi=softfp"
 ./configure --prefix=/usr --disable-static --build=i686-pc-linux-gnu --host=arm-linux-androideabi
-make -j8 2>&1 | tee armeabi-v7a.log
+make -j8 V=1 2>&1 | tee armeabi-v7a.log
 make install DESTDIR=$PWD/armeabi-v7a
 cd armeabi-v7a && mv usr/lib/libgmp.so usr/include/gmp.h . && rm -rf usr && cd ..
 make distclean
 
 # armeabi
-export CFLAGS="-O2 -pedantic -fomit-frame-pointer"
+export CFLAGS="${BASE_CFLAGS}"
 ./configure --prefix=/usr --disable-static --build=i686-pc-linux-gnu --host=arm-linux-androideabi
-make -j8 2>&1 | tee armeabi.log
+make -j8 V=1 2>&1 | tee armeabi.log
 make install DESTDIR=$PWD/armeabi
 cd armeabi && mv usr/lib/libgmp.so usr/include/gmp.h . && rm -rf usr && cd ..
 make distclean
