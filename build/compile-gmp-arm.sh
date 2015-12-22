0708b1b485e84baaa78320b5f2b089ea3e1ce926
Michael Mohr <akihana@gmail.com>
2015-12-22 00:26:54 -0800

    Make MPN_PATH explicit for ARM64

--- build/compile-gmp-arm.sh
+++ build/compile-gmp-arm.sh
@@ -50,7 +50,7 @@ export LDFLAGS='-Wl,--no-undefined -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now'
 # arm64-v8a
 echo "======= COMPILING FOR arm64-v8a ======="
 export CFLAGS="${BASE_CFLAGS} -fstack-protector-strong -finline-limit=300 -funswitch-loops"
-./configure --prefix=/usr --disable-static ${CPLUSPLUS_FLAGS} --build=x86_64-pc-linux-gnu --host=aarch64-linux-android
+./configure --prefix=/usr --disable-static ${CPLUSPLUS_FLAGS} --build=x86_64-pc-linux-gnu --host=aarch64-linux-android MPN_PATH="arm64 generic"
 make -j8 V=1 2>&1 | tee arm64-v8a.log
 make install DESTDIR=$PWD/arm64-v8a
 cd arm64-v8a && mv usr/lib/libgmp.so usr/lib/libgmpxx.so usr/include/gmp.h usr/include/gmpxx.h . && rm -rf usr && cd ..
