ecb489384addead1ef4d7d264fa75f9bcea4678b
Michael Mohr <akihana@gmail.com>
2015-12-29 00:21:10 -0800

    Unconditionally build C++ bindings for MIPS

--- build/compile-gmp-mips.sh
+++ build/compile-gmp-mips.sh
@@ -27,9 +27,6 @@ export LDFLAGS='-Wl,--no-undefined -Wl,-z,noexecstack -Wl,-z,relro -Wl,-z,now'
 export LIBGMP_LDFLAGS='-avoid-version'
 export LIBGMPXX_LDFLAGS='-avoid-version'
 
-# The following line enables C++ support. For GMP <= 5.1.2 you *must* apply gmp_decimal_point.patch prior to running this script.
-export CPLUSPLUS_FLAGS='--enable-cxx'
-
 ################################################################################################################
 
 # base CFLAGS set from ndk-build output
@@ -37,14 +34,9 @@ BASE_CFLAGS='-O2 -g -pedantic -fomit-frame-pointer -Wa,--noexecstack -fno-strict
 
 # mips CFLAGS not specified in 'CPU Arch ABIs' in the NDK documentation
 export CFLAGS="${BASE_CFLAGS}"
-./configure --prefix=/usr --disable-static ${CPLUSPLUS_FLAGS} --build=x86_64-pc-linux-gnu --host=mipsel-linux-android
+./configure --prefix=/usr --disable-static --enable-cxx --build=x86_64-pc-linux-gnu --host=mipsel-linux-android
 make -j8 V=1 2>&1 | tee android-mips.log
 make install DESTDIR=$PWD/mips
-if [ -z "${CPLUSPLUS_FLAGS}" ]
-then
-  cd mips && mv usr/lib/libgmp.so usr/include/gmp.h . && rm -rf usr && cd ..
-else
-  cd mips && mv usr/lib/libgmp.so usr/lib/libgmpxx.so usr/include/gmp.h usr/include/gmpxx.h . && rm -rf usr && cd ..
-fi
+cd mips && mv usr/lib/libgmp.so usr/lib/libgmpxx.so usr/include/gmp.h usr/include/gmpxx.h . && rm -rf usr && cd ..
 make distclean
 exit 0
