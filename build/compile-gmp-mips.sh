cbbc7c75e5f9fb72854675291c30667ef731f495
Michael Mohr <akihana@gmail.com>
2017-05-22 20:44:47 -0700

    Update NDK and API targets in build scripts

--- build/compile-gmp-mips.sh
+++ build/compile-gmp-mips.sh
@@ -6,7 +6,7 @@ then
   exit 1
 fi
 
-export NDK="/tmp/android-ndk-r13b"
+export NDK="/tmp/android-ndk-r14b"
 if [ ! -d ${NDK} ]
 then
   echo "Please download and install the NDK, then update the path in this script."
@@ -15,14 +15,14 @@ then
 fi
 
 # Extract an android-14 toolchain if needed
-export TARGET32="android-19"
+export TARGET32="android-24"
 export TOOLCHAIN32="/tmp/${TARGET32}-mips"
 if [ ! -d ${TOOLCHAIN32} ]
 then
   ${NDK}/build/tools/make-standalone-toolchain.sh --toolchain=mipsel-linux-android-4.9 --platform=${TARGET32} --install-dir=${TOOLCHAIN32} || exit 1
 fi
 
-export TARGET64="android-21"
+export TARGET64="android-24"
 export TOOLCHAIN64="/tmp/${TARGET64}-mips64"
 if [ ! -d ${TOOLCHAIN64} ]
 then
@@ -42,6 +42,7 @@ BASE_CFLAGS='-O2 -g -pedantic -fomit-frame-pointer -Wa,--noexecstack -fno-strict
 # mips CFLAGS not specified in 'CPU Arch ABIs' in the NDK documentation
 export CFLAGS="${BASE_CFLAGS}"
 ./configure --prefix=/usr --disable-static --enable-cxx --build=x86_64-pc-linux-gnu --host=mipsel-linux-android MPN_PATH="mips32 generic"
+sed -i.bak '/HAVE_LOCALECONV 1/d' ./config.h
 make -j8 V=1 2>&1 | tee android-mips.log
 make install DESTDIR=$PWD/mips
 cd mips && mv usr/lib/libgmp.so usr/lib/libgmpxx.so usr/include/gmp.h usr/include/gmpxx.h . && rm -rf usr && cd ..
@@ -52,6 +53,7 @@ export CFLAGS="${BASE_CFLAGS}"
 # Ugly hack: delete incompatible assembly code from MPN directory
 rm -f mpn/mips64/{addmul_1.asm,submul_1.asm,mul_1.asm,sqr_diagonal.asm,umul.asm}
 ./configure --prefix=/usr --disable-static --enable-cxx --build=x86_64-pc-linux-gnu --host=mips64el-linux-android MPN_PATH="mips64 generic"
+sed -i.bak '/HAVE_LOCALECONV 1/d' ./config.h
 make -j8 V=1 2>&1 | tee android-mips64.log
 make install DESTDIR=$PWD/mips64
 cd mips64 && mv usr/lib/libgmp.so usr/lib/libgmpxx.so usr/include/gmp.h usr/include/gmpxx.h . && rm -rf usr && cd ..
--- build/compile-gmp-x86.sh
+++ build/compile-gmp-x86.sh
@@ -6,7 +6,7 @@ then
   exit 1
 fi
 
-export NDK="/tmp/android-ndk-r13b"
+export NDK="/tmp/android-ndk-r14b"
 if [ ! -d ${NDK} ]
 then
   echo "Please download and install the NDK, then update the path in this script."
@@ -15,14 +15,14 @@ then
 fi
 
 # Extract an android toolchain if needed
-export TARGET32="android-19"
+export TARGET32="android-24"
 export TOOLCHAIN32="/tmp/${TARGET32}-x86"
 if [ ! -d ${TOOLCHAIN32} ]
 then
   ${NDK}/build/tools/make-standalone-toolchain.sh --toolchain=x86-4.9 --platform=${TARGET32} --install-dir=${TOOLCHAIN32} || exit 1
 fi
 
-export TARGET64="android-21"
+export TARGET64="android-24"
 export TOOLCHAIN64="/tmp/${TARGET64}-x86_64"
 if [ ! -d ${TOOLCHAIN64} ]
 then
@@ -42,6 +42,7 @@ BASE_CFLAGS='-O2 -g -pedantic -Wa,--noexecstack -fomit-frame-pointer -ffunction-
 # x86, CFLAGS set according to 'CPU Arch ABIs' in the r8c documentation
 export CFLAGS="${BASE_CFLAGS} -fstack-protector -march=i686 -mtune=intel -mssse3 -mfpmath=sse -m32"
 ./configure --prefix=/usr --disable-static --enable-cxx --build=x86_64-pc-linux-gnu --host=i686-linux-android MPN_PATH="x86/atom/sse2 x86/atom/mmx x86/atom x86/mmx x86 generic" || exit 1
+sed -i.bak '/HAVE_LOCALECONV 1/d' ./config.h
 make -j8 V=1 2>&1 | tee android-x86.log
 make install DESTDIR=$PWD/x86
 cd x86 && mv usr/lib/libgmp.so usr/lib/libgmpxx.so usr/include/gmp.h usr/include/gmpxx.h . && rm -rf usr && cd ..
@@ -51,6 +52,7 @@ make distclean
 export CFLAGS="${BASE_CFLAGS} -fstack-protector-strong -march=x86-64 -msse4.2 -mpopcnt -m64 -mtune=intel"
 
 ./configure --prefix=/usr --disable-static --enable-cxx --build=x86_64-pc-linux-gnu --host=x86_64-linux-android MPN_PATH="x86_64/pentium4 x86_64/fastsse x86_64/k8 x86_64 generic" || exit 1
+sed -i.bak '/HAVE_LOCALECONV 1/d' ./config.h
 make -j8 V=1 2>&1 | tee android-x86_64.log
 make install DESTDIR=$PWD/x86_64
 cd x86_64 && mv usr/lib/libgmp.so usr/lib/libgmpxx.so usr/include/gmp.h usr/include/gmpxx.h . && rm -rf usr && cd ..
